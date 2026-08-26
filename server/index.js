50dec3ff1bf7d9fc04d3763970513279ebe34e6b
LaxinGroup <LaxiNkulul3ko@gmail.com>
2026-08-26 09:14:30 +0200

    second commit

--- server/index.js
+++ server/index.js
@@ -84,4 +84,4 @@ app.get('/api/me', requireAuth, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
